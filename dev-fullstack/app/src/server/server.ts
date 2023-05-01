229e000663e97aaf09c5a1c6d97dca3f1ca946e5
Rositca Yancheva <rositcayancheva@gmail.com>
2023-05-01 21:39:04 +0200

    feat: set up apollo server

---
+++ dev-fullstack/app/src/server/server.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import {
+  Context,
+  createContext,
+} from './context';
+import { schema } from './schema';
+
+const start = async () => {
+  const server = new ApolloServer<Context>({ schema });
+
+  const { url } = await startStandaloneServer(server, {
+    context: createContext,
+    listen: { port: 4000 },
+  });
+
+  console.log(`\
+  🚀 Server ready at: ${url}
+  `);
+};
+
+start();
